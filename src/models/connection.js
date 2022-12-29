61019df7b21f95a8b215e1572fb3eb707b0745f1
jean <mendes.jean.cs@gmail.com>
2022-12-29 09:07:38 -0300

    add MONGODB_URL on dotenv

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,10 +1,13 @@
 const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
 let dbConnection;
 
+const url = `${process.env.MONGODB_URL}/bookstore`;
+
 module.exports = {
   connectToDb: (cb) => {
-    MongoClient.connect("mongodb://localhost:27017/bookstore")
+    MongoClient.connect(url)
       .then((client) => {
         dbConnection = client.db();
         return cb();
